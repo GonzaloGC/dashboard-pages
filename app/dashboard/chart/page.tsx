21752f772f245399c9ed66e85390decf0e520f51
Gonzalo <elias.gonzaloguerrero@gmail.com>
2024-07-28 21:11:50 -0400

    added layout design

---
+++ app/dashboard/chart/page.tsx
@@ -0,0 +1,5 @@
+const chartPage = () => {
+  return <h1>este es un gráfico</h1>;
+};
+
+export default chartPage;
---
+++ app/ui/customers/page.tsx
@@ -0,0 +1,5 @@
+const CustomerPage = () => {
+  return <h1>esta es la pagina de customers</h1>;
+};
+
+export default CustomerPage;
