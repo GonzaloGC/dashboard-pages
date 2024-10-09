69cf73a4e2b5d48bbe8e34d40f4c7970ea716a09
Gonzalo Guerrero <elias.gonzaloguerrero@gmail.com>
2024-10-08 22:36:34 -0300

    changed logo

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,12 +1,19 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 // import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
     <div
       className={`flex flex-row items-end gap-4 leading-none text-white`}
     >
-      <GlobeAltIcon className="h-7 w-7 rotate-[15deg] p-0 m-0" />
+      {/* <GlobeAltIcon className="h-7 w-7 rotate-[15deg] p-0 m-0" /> */}
+      <Image
+      src="/logo-512x512 -.png"
+      alt='logo gonzalo guerrero'
+      width={50}
+      height={20}
+      />
       <p className="text-[44px]">GG</p>
     </div>
   );
